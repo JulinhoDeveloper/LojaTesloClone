27fa30dc1bbde195464f5ef7c148e0236c21a88a
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-03-03 19:22:28 -0300

    criando a navbar

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import Head from 'next/head';
 
+import { Navbar } from '../ui';
+
 
 interface Props {
     title: string;
@@ -29,7 +31,7 @@ export const ShopLayout:FC<Props> = ({ children, title, pageDescription, imageFu
         </Head> 
 
         <nav>
-         
+        <Navbar />
         </nav>
 
        
@@ -41,6 +43,7 @@ export const ShopLayout:FC<Props> = ({ children, title, pageDescription, imageFu
         }}>
             { children }
         </main>
+
 
         <footer>
            
