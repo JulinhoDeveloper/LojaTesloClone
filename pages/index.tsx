8dcc49d6208d7e51c7fcb62ea7a755b1a1db8bb7
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-03-04 06:29:54 -0300

    mostrando os produtos na home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next';
-import { Card, CardActionArea, CardMedia, Typography , Grid} from '@mui/material';
+import { Typography } from '@mui/material';
 
 import { ShopLayout}  from '../components/layouts';
 import { initialData } from '../database/products';
+import { ProductList } from '../components/products';
 
 
 
@@ -13,23 +14,10 @@ const Home: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>Todos os produtos</Typography>
   
 
-<Grid container spacing={4}>
-{
-initialData.products.map(  product => (
-<Grid item xs={6} sm={4} key={ product.slug }>
-<Card>
-<CardActionArea>
-<CardMedia
-component='img'
-image={ `products/${ product.images[0] }` }
-alt={ product.title }
-/>
-  </CardActionArea>
-</Card>
-</Grid>
-))
-}
-</Grid>
+      <ProductList
+      products={ initialData.products as any }
+      />
+
 </ShopLayout>
   )
 }
