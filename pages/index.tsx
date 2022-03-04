1dafccce71bfb7a3bc280170959016bfd607291b
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-03-03 22:29:30 -0300

    home temporária

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
-import { ShopLayout}  from '../components/layouts';
+import { Card, CardActionArea, CardMedia, Typography , Grid} from '@mui/material';
 
+import { ShopLayout}  from '../components/layouts';
+import { initialData } from '../database/products';
 
 
 
@@ -10,7 +11,26 @@ const Home: NextPage = () => {
     <ShopLayout title={'Teslo Shop -  Home'} pageDescription={'Os melhores produtos'}>
       <Typography variant='h1' component='h1'>Loja</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos os produtos</Typography>
-    </ShopLayout>
+  
+
+<Grid container spacing={4}>
+{
+initialData.products.map(  product => (
+<Grid item xs={6} sm={4} key={ product.slug }>
+<Card>
+<CardActionArea>
+<CardMedia
+component='img'
+image={ `products/${ product.images[0] }` }
+alt={ product.title }
+/>
+  </CardActionArea>
+</Card>
+</Grid>
+))
+}
+</Grid>
+</ShopLayout>
   )
 }
 
